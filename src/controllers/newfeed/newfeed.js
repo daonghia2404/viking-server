9666920c4ef4a4f33af56e58417c2b2f039bbbc3
daonghia2404 <daonghia2404@gmail.com>
2021-01-19 22:52:11 +0700

    Intergate Media and Tag Newfeed

--- src/controllers/newfeed/newfeed.js
+++ src/controllers/newfeed/newfeed.js
@@ -1,12 +1,25 @@
 const User = require('../../models/user/user')
+const {
+  validateUrlImage,
+  validateUrlVideo,
+  validateUrlAudio,
+  validateText,
+} = require('../../utils/validation')
 
-const showDataReaction = async (dataReaction) => {
+const showDataReaction = async (dataReaction, currentUser) => {
   const reactions = []
+  let isUserReacted = {}
   await Promise.all(
     dataReaction.data.map(async (reaction) => {
       const targetUserReaction = await User.findOne({
         username: reaction.username,
       })
+
+      if (currentUser === targetUserReaction.username)
+        isUserReacted = {
+          type: reaction.type,
+          username: targetUserReaction.username,
+        }
 
       reactions.push({
         type: reaction.type,
@@ -15,17 +28,25 @@ const showDataReaction = async (dataReaction) => {
       })
     })
   )
-  return reactions
+
+  return {
+    total: dataReaction.totals,
+    data: reactions,
+    isUserReacted,
+  }
 }
 
-const showCommentData = async (dataComments) => {
+const showCommentData = async (dataComments, currentUser) => {
   const comments = []
   await Promise.all(
     dataComments.map(async (comment) => {
+      const reactions = await showDataReaction(comment.reactions, currentUser)
+      const medias = await showMediaData(comment.medias)
       comments.push({
         caption: comment.caption,
         id: comment._id,
-        media: comment.media,
+        medias,
+        reactions,
         userCreateComment: comment.userCreateComment,
         createdAt: comment.createdAt,
       })
@@ -34,7 +55,22 @@ const showCommentData = async (dataComments) => {
   return comments
 }
 
-const showDataNewfeed = async (targetNewFeeds) => {
+const showMediaData = async (dataMedias) => {
+  const medias = []
+  await Promise.all(
+    dataMedias.map(async (media) => {
+      medias.push({
+        id: media._id,
+        url: media.url,
+        type: media.type,
+        fileName: media.fileName,
+      })
+    })
+  )
+  return medias
+}
+
+const showDataNewfeed = async (targetNewFeeds, currentUser) => {
   const data = []
   await Promise.all(
     targetNewFeeds.map(async (item) => {
@@ -43,22 +79,18 @@ const showDataNewfeed = async (targetNewFeeds) => {
       })
 
       if (targetUserPost) {
-        const reactions = await showDataReaction(item.reactions)
-        const comments = await showCommentData(item.comments)
-        const tags = []
-        const medias = []
+        const reactions = await showDataReaction(item.reactions, currentUser)
+        const comments = await showCommentData(item.comments, currentUser)
+        const medias = await showMediaData(item.medias)
 
         data.push({
           id: item._id,
           type: item.type,
           caption: item.caption,
-          medias,
-          reactions: {
-            total: item.reactions.totals,
-            data: reactions,
-          },
+          medias: medias || [],
+          reactions,
           comments,
-          tags,
+          tags: item.tags,
           createdAt: item.createdAt,
           userCreatePost: {
             username: targetUserPost.username,
@@ -162,23 +194,22 @@ const updateReaction = (newReactionData, oldReaction) => {
 const createComment = async (commentData, commentsOld) => {
   const data = commentsOld
   const targetUserPost = await User.findOne({ username: commentData.username })
-  const reactions = await showDataReaction({
-    totals: {
-      likes: 0,
-      loves: 0,
-      wows: 0,
-      hahas: 0,
-      sads: 0,
-      angrys: 0,
-      totalReactions: 0,
-    },
-    data: [],
-  })
 
   data.push({
     caption: commentData.caption,
-    media: commentData.media,
-    reactions,
+    medias: commentData.medias,
+    reactions: {
+      totals: {
+        likes: 0,
+        loves: 0,
+        wows: 0,
+        hahas: 0,
+        sads: 0,
+        angrys: 0,
+        totalReactions: 0,
+      },
+      data: [],
+    },
     userCreateComment: {
       username: targetUserPost.username,
       avatar: targetUserPost.avatar || null,
@@ -195,21 +226,110 @@ const updateComment = async (commentData, commentsOld) => {
     index: -1,
   }
   data.map((item, index) => {
-    if (item.userCreateComment.username === commentData.username) {
+    if (item._id.toString() === commentData.commentId) {
       commentTarget.data = item
       commentTarget.index = index
     }
   })
   if (commentTarget.data.id) {
-    data[commentTarget.index] = {
-      userCreateComment: commentTarget.data.userCreateComment,
-      createdAt: commentTarget.data.createdAt,
-      caption: commentData.caption,
-      media: commentData.media,
+    if (
+      commentTarget.data.userCreateComment.username === commentData.username
+    ) {
+      data[commentTarget.index] = {
+        userCreateComment: commentTarget.data.userCreateComment,
+        createdAt: commentTarget.data.createdAt,
+        reactions: commentTarget.data.reactions,
+        caption: commentData.caption,
+        medias: commentData.medias,
+      }
+      return data
+    } else {
+      return 'PERMISSION'
     }
+  } else {
+    return 'NOT_FOUND'
+  }
+}
+
+const deleteComment = async (commentData, commentsOld, newfeed) => {
+  const data = commentsOld
+  const commentTarget = {
+    data: {},
+    index: -1,
+  }
+  data.map((item, index) => {
+    if (item._id.toString() === commentData.commentId) {
+      commentTarget.data = item
+      commentTarget.index = index
+    }
+  })
+  if (commentTarget.data.id) {
+    if (
+      commentTarget.data.userCreateComment.username === commentData.username ||
+      newfeed.userCreatePost.username === commentData.username
+    ) {
+      data.splice(commentTarget.index, 1)
+      return data
+    } else {
+      return 'PERMISSION'
+    }
+  } else {
+    return 'NOT_FOUND'
+  }
+}
+
+const reactionComment = async (newReactionData, oldComment) => {
+  const data = oldComment
+  const commentTarget = {
+    data: {},
+    index: -1,
+  }
+  data.map((item, index) => {
+    if (item._id.toString() === newReactionData.commentId) {
+      commentTarget.data = item
+      commentTarget.index = index
+    }
+  })
+  if (commentTarget.data.id) {
+    const targetCommentReaction = commentTarget.data.reactions
+    const newReaction = updateReaction(newReactionData, targetCommentReaction)
+    data[commentTarget.index].reactions = newReaction
     return data
   } else {
-    return false
+    return 'NOT_FOUND'
+  }
+}
+
+const checkTypeMedia = (req, res, next) => {
+  const dataMedia = req.body.medias
+  if (dataMedia.length !== 0) {
+    let error = ''
+    dataMedia.map((item) => {
+      if (!validateText(item.fileName)) {
+        error = 'Không có Filename'
+      }
+      if (!['IMAGE', 'VIDEO', 'AUDIO'].includes(item.type)) {
+        error = 'Type không hợp lệ'
+      }
+      if (item.type === 'IMAGE' && !validateUrlImage(item.url)) {
+        error = 'URL image không hợp lệ'
+      }
+      if (item.type === 'VIDEO' && !validateUrlVideo(item.url)) {
+        error = 'URL video không hợp lệ'
+      }
+      if (item.type === 'AUDIO' && !validateUrlAudio(item.url)) {
+        error = 'URL audio không hợp lệ'
+      }
+    })
+    if (!error) next()
+    else {
+      res.json({
+        success: false,
+        message: error,
+      })
+    }
+  } else {
+    next()
   }
 }
 
@@ -219,4 +339,7 @@ module.exports = {
   checkTypeReaction,
   createComment,
   updateComment,
+  deleteComment,
+  reactionComment,
+  checkTypeMedia,
 }
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -5,4 +5,24 @@ const validateFileSize = (file, size = FILE_SIZE) => {
   return fileSize < size
 }
 
-module.exports = { validateFileSize }
+const validateText = (value) => {
+  return value && value.trim() !== ''
+}
+
+const validateUrlImage = (url) => {
+  return /\.(jpeg|jpg|gif|png)$/.test(url)
+}
+const validateUrlAudio = (url) => {
+  return /\.(?:wav|mp3)$/.test(url)
+}
+const validateUrlVideo = (url) => {
+  return /\.(mp4|mov)$/.test(url)
+}
+
+module.exports = {
+  validateFileSize,
+  validateText,
+  validateUrlImage,
+  validateUrlAudio,
+  validateUrlVideo,
+}
