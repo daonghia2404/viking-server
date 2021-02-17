7df763f10b1d44160505e437351ddbd778c35f71
daonghia2404 <daonghia2404@gmail.com>
2021-02-17 10:02:22 +0700

    test

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ app.use(uploadsRoute)
 app.use(newfeedRoute)
 
 // Deploy
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'))
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
